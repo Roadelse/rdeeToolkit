40a3b7e70f8f81879f8a40778636695b017e5285
rdee.ZJDesktop <roadelse@qq.com>
2024-03-16 11:02:04 +0800

    minor

--- python/rdee/_win.py
+++ python/rdee/_win.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 
+import re
+import os.path
+import platform
 
 #**********************************************************************
 # this function is used to create shortcut
@@ -43,14 +46,35 @@ def GetShortCut(shortcut):
     return shell.CreateShortCut(shortcut).Targetpath
 
 
+#@ func::path_win2wsl
 def path_win2wsl(path_win):
     strT = path_win[3:].replace('\\', '/')
     path_wsl = f"/mnt/{path_win[0].lower()}/{strT}"
     return path_wsl
 
-def path_wsl2win(path_wsl):
+
+#@ func::path_wsl2win
+def path_wsl2win(path_wsl: str, require_existed: bool = False):
+    from ._x_logging import logger
+
+    if not re.match(r'/mnt/[a-z]/', path_wsl):
+        logger.error(f"The arg:path_wsl is not a wsl path for windows: {path_wsl}")
+        raise RuntimeError
+    
+
     disk = path_wsl[5]
     path_win = disk.upper() + ":\\" + path_wsl[7:].replace('/', '\\')
+    
+    if require_existed:
+        if platform.system() == "Linux":
+            if not os.path.exists(path_wsl):
+                logger.error(f"require_existed=True, but target path doesn't exist! {path_wsl}")
+                raise RuntimeError
+        else:
+            if not os.path.exists(path_win):
+                logger.error(f"require_existed=True, but target path doesn't exist! {path_win}")
+                raise RuntimeError
+
     return path_win 
 
 
